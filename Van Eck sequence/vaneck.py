dc492e66e659a6eed2732b4cf71c70e7f05753f7
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-19 23:51:07 +0200

    added comments

--- Van Eck sequence/vaneck.py
+++ Van Eck sequence/vaneck.py
@@ -1,24 +1,22 @@
-n=int(input('enter n'))
-lst=[]
-a=0
-ch=''
+n = int(input('Enter n: '))  # Number of terms in the sequence
+lst = []                      # List to store tuples (index, value)
+a = 0                          # Current term
+ch = ''                        # String to build the output sequence
+
 for i in range(n):
-    if lst == []:
-        lst.append((i,0))
-        ch = ch + " " + str(a)
+    if lst == []:  # First element
+        lst.append((i, 0))
+        ch += str(a) + " "
     else:
-        second_elements = [t[1] for t in lst]
-        prev = lst[-1][1]  # previous term
-        if prev not in second_elements[:-1]:  # exclude last
+        second_elements = [t[1] for t in lst]  # All previous values
+        prev = lst[-1][1]                       # Previous term
+        if prev not in second_elements[:-1]:    # If prev never appeared before
             a = 0
         else:
+            # Find last occurrence of prev before the previous term
             j = len(lst) - 2 - second_elements[:-1][::-1].index(prev)
             a = i - 1 - j
-        lst.append((i,a))
-        ch = ch + " " + str(a)
+        lst.append((i, a))
+        ch += str(a) + " "
 
-print(ch.strip())
-
-
-        
-
+print(ch.strip())  # Print the sequence as space-separated numbers
